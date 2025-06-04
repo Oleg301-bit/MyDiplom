3c9c94c0e2df96b68f56c46099a7836e3a98bc18
Oleg <olegmaslakov47@gmail.com>
2025-06-04 11:39:07 +0300

    Add new pages

--- test-main/pay.js
+++ test-main/pay.js
@@ -2,9 +2,23 @@ const cityInput = document.getElementById('city');
 const districtInput = document.getElementById('district');
 const branchOutput = document.getElementById('branch');
 const orderNumberOutput = document.getElementById('orderNumber');
+const pickupTimeOutput = document.getElementById('pickupTime');
 
 function generateRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomDate(startDate, endDate) {
+  const start = startDate.getTime();
+  const end = endDate.getTime();
+  const randomTimestamp = generateRandomNumber(start, end);
+  return new Date(randomTimestamp);
+}
+
+function getRandomTime() {
+  const hour = generateRandomNumber(9, 19);
+  const minute = generateRandomNumber(0, 1) === 0 ? '00' : '30';
+  return `${hour.toString().padStart(2, '0')}:${minute}`;
 }
 
 function tryGenerate() {
@@ -15,11 +29,25 @@ function tryGenerate() {
     const branchNumber = generateRandomNumber(1, 50);
     const orderNum = generateRandomNumber(100000, 999999);
 
-    branchOutput.textContent = `Найближче відділення нової пошти №${branchNumber}`;
-    orderNumberOutput.textContent = `Номер вашого замовлення: ${orderNum}`;
+    const randomDate = getRandomDate(
+      new Date('2025-05-06'),
+      new Date('2025-05-29')
+    );
+    const formattedDate = `${randomDate
+      .getDate()
+      .toString()
+      .padStart(2, '0')}.${(randomDate.getMonth() + 1)
+      .toString()
+      .padStart(2, '0')}.${randomDate.getFullYear()}`;
+    const time = getRandomTime();
+
+    branchOutput.textContent = `Найближче відділення нової пошти №${branchNumber}.`;
+    orderNumberOutput.textContent = `Номер вашого замовлення: ${orderNum}.`;
+    pickupTimeOutput.textContent = `Замовлення буде Вас очікувати: ${formattedDate}  о ${time}. Ви можете забрати товар впродовж 3 днів.`;
   } else {
     branchOutput.textContent = '';
     orderNumberOutput.textContent = '';
+    pickupTimeOutput.textContent = '';
   }
 }
 
